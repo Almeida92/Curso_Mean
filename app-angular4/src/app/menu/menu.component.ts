6a7ecd90eec1484ceaeba6aaccb84b660e037448
Felipe Almeida <felipepereira937@gmail.com>
2017-11-15 15:13:59 -0200

    interpolação

--- app-angular4/src/app/menu/menu.component.ts
+++ app-angular4/src/app/menu/menu.component.ts
@@ -4,4 +4,8 @@ import { Component } from '@angular/core';
     selector: 'menu',
     templateUrl: 'views/menu.component.html'
 })
-export class MenuComponent { }
+export class MenuComponent {
+    titulo_escola: string = 'Impacta Treinamentos';
+    titulo_cad: string = 'Cadastro de Cursos';
+    titulo_lista = 'Listagem de cursos';
+ }
