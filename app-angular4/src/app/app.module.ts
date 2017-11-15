87f814e3e93c0730d7428c704686cf956bc29de6
Felipe Almeida <felipepereira937@gmail.com>
2017-11-15 15:39:34 -0200

    criando components de Rotas

--- app-angular4/src/app/app.module.ts
+++ app-angular4/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-
+import { appRoutes } from './rotas/app.routes';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 
+// usando as rotas
+import { LogoComponent } from './logo/logo.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListaComponent } from './lista/lista.component';
+import { NotFoundComponent } from './erro/erro.notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent, // componentes usados na aplicação
-    MenuComponent
-  ],
+    MenuComponent,
+    LogoComponent,
+    CadastroComponent,
+    ListaComponent,
+    NotFoundComponent],
   imports: [
-    BrowserModule // módulos usados na aplicação
+    BrowserModule, // módulos usados na aplicação
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent] // Indica qual o componente inicial da aplicação
