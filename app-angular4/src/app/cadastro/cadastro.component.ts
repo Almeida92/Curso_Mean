4de9e775838ffc460fff5518b3d7c02c8cceadba
Felipe Almeida <felipepereira937@gmail.com>
2017-11-18 12:54:47 -0200

    Criando Formulário

--- app-angular4/src/app/cadastro/cadastro.component.ts
+++ app-angular4/src/app/cadastro/cadastro.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 import { ICurso } from '../classes/interface.curso';
 
 @Component({
-    template: `
-        <div class="container margem"> 
-            <h1> CADASTRO DE CURSOS</h1>
-            <h3>Código: {{ curso.codigo }}</h3>
-            <h3>Descrição: {{ curso.descricao }}</h3>
-            <h3>Carga Horária: {{ curso.ch }}</h3>
-        </div>
-    `
+    templateUrl: './views/cadastro.component.html'
 })
 export class CadastroComponent {
     // simulação: definindo apenas um curso
